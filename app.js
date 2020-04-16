cb01dee18c117f3c6a446dd806dca1f4f517060a
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-04-16 16:31:48 -0400

    initial call for the listener when clicking the submission button 'calculate'

---
+++ app.js
@@ -0,0 +1,23 @@
+// const loanAmount = document.querySelector('#amount');
+// const interestAmount = document.querySelector('#amount');
+// const yearToRepay = document.querySelector('#amount');
+// const calcBtn = document.querySelector('#amount');
+// const totalMonthly = document.querySelector('#amount');
+// const totalPayment = document.querySelector('#amount');
+// const totalInterest = document.querySelector('#amount');
+
+// loadEventListener();
+
+// function loadEventListener(){
+   
+// }
+
+// LISTEN FOR SUBMISSION
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// CALCULATE RESULTS FUNCTION
+function  calculateResults(e){
+console.log('calculating')
+
+    e.preventDefault();
+}
